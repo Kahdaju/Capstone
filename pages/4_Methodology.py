08ffe32d7f70b8e7e4abc7e508f622faa30328bd
Kahdaju <sgt3fsj@gmail.com>
2024-10-26 17:14:00 +0800

    Update 4_Methodology.py

--- pages/4_Methodology.py
+++ pages/4_Methodology.py
@@ -17,7 +17,7 @@ col1,col2 = st.columns(2)
 
 with col1:
     # st.header("CPF Retirement Query")
-    st.image ("images\Query.png", caption="CPF Retirement Query",output_format="png")
+    st.image ("images/Query.png", caption="CPF Retirement Query",output_format="png")
 with col2:
     # st.header("Retirement Planner")
-    st.image ("images\Retirement Planner.png", caption="Retirement Planner",output_format="png")
+    st.image ("images/Retirement Planner.png", caption="Retirement Planner",output_format="png")
